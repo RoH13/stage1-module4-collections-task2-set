e92c6c17007ad60c76b4ff0fabbbb29770ad3884
raman <r4m4n911@gmail.com>
2025-07-24 14:23:52 +0300

    gfd

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -1,9 +1,25 @@
 package com.epam.mjc.collections.set;
 
+
 import java.util.HashSet;
 import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet <Integer> set = new HashSet<Integer>();
+        for (int c : sourceList) {
+            set.add(c);
+            if (c % 2 != 0 ) {
+                
+                set.add(2*c);
+            } else {
+                while (c % 2 == 0) {
+                    c /= 2;
+                    set.add(c);
+                }
+            }
+        }
+        return set;
     }
+    
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,19 @@
 package com.epam.mjc.collections.set;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        Set <Integer> set = new HashSet<Integer>();
+        for (int x : sourceList) {
+            if (x * x >= lowerBound & x * x <= upperBound) {
+                set.add(x*x);
+            }
+        }
+    
+    return set;
     }
+   
 }
